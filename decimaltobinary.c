68f0c519d3d70d223952b21604c654986afd545a
KAVIYAA218 <kaviyaa957@gmail.com>
2022-10-17 10:07:14 -0700

    task

---
+++ decimaltobinary.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+//#include<stdlib>
+int main()
+{
+    int a[10],n,i;
+    scanf("%d",&n);
+    for(i=0;n>0;i++)
+    {
+        a[i]=n%2;
+        n=n/2;
+    }
+    for(i=i-1;i>=0;i--)
+    {
+       if(a[i]==1)
+       {
+        printf("%d ",i);
+       }
+    }
+    return 0;
+}
